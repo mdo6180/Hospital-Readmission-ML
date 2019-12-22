2b6c7154e0da5f50d676b0e922ad66829f417088
Minh Quan Do <mdo6180@gmail.com>
2019-12-22 06:11:24 -0500

    balancing dataset by removing 'no readmission' data points and 23,000 '>30' data points

--- Data_Exploration.py
+++ Data_Exploration.py
@@ -8,7 +8,7 @@ dataset = read_csv('./dataset_diabetes/diabetic_data.csv')
 # dropping 'encounter_id', 'patient_nbr' because fields are just identifiers
 dataset = dataset.drop(columns=['encounter_id', 'patient_nbr', 'weight',
                                 'payer_code', 'medical_specialty', 'change', 'diabetesMed'])
-
+'''
 num_less_than_30 = len(dataset[dataset['readmitted'] == '<30'].index)
 num_more_than_30 = len(dataset[dataset['readmitted'] == '>30'].index)
 num_not_readmitted = len(dataset[dataset['readmitted'] == 'NO'].index)
@@ -21,17 +21,30 @@ plt.ylabel('Number of Patients', labelpad=5)
 plt.xlabel('Time Frame of Readmission', labelpad=5)
 plt.xticks(ind, ('<30 Days', '>30 Days', 'Not Readmitted'))
 plt.show()
-
 '''
+
+
 # dropping data point if patient has no record of readmission
 NO_index = dataset[dataset['readmitted'] == 'NO'].index
 dataset = dataset.drop(index=NO_index)
 
+# balance dataset by dropping 23,000 '>30' data points to reduce data set to a little over 12,000 '>30' data points
 over_30 = dataset[dataset['readmitted'] == '>30'].index
-over_30 = over_30[0:13000]
+over_30 = over_30[0:23000]
 dataset = dataset.drop(index=over_30)
 
-'''
+dataset = dataset.sample(frac=1, random_state=1)    # shuffle data set
 
 
+num_less_than_30 = len(dataset[dataset['readmitted'] == '<30'].index)
+num_more_than_30 = len(dataset[dataset['readmitted'] == '>30'].index)
+labels_stats = [num_less_than_30, num_more_than_30]
+ind = np.arange(len(labels_stats))
+
+plt.bar(ind, labels_stats, align='center')
+plt.title('Hospital Readmission Statistics')
+plt.ylabel('Number of Patients', labelpad=5)
+plt.xlabel('Time Frame of Readmission', labelpad=5)
+plt.xticks(ind, ('<30 Days', '>30 Days'))
+plt.show()
 
